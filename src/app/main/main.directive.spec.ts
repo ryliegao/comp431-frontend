44831dd88658ceb0efde4e8b75072d5de4358edc
gyuanrui <yg38@rice.edu>
2019-03-08 03:22:34 -0600

    Rebuild everything.

--- src/app/main/main.directive.spec.ts
+++ src/app/main/main.directive.spec.ts
@@ -1,19 +1,25 @@
-import { MainDirective } from './main.directive';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ElementRef } from '@angular/core';
-import {TestBed} from '@angular/core/testing';
+import { MainDirective } from 'src/app/main/main.directive';
+import { MainComponent } from 'src/app/main/main.component';
+import { StorageService } from 'src/app/_services';
 
 describe('MainDirective', () => {
   let elementRef: ElementRef;
+  let component: MainComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: ElementRef }]
+      imports: [ HttpClientTestingModule ],
+      providers: [ MainComponent, StorageService, { provide: ElementRef } ]
     });
     elementRef = TestBed.get(ElementRef);
+    component = TestBed.get(MainComponent);
   });
 
   it('should create an instance', () => {
-    const directive = new MainDirective(elementRef);
+    const directive = new MainDirective(elementRef, component);
     expect(directive).toBeTruthy();
   });
 });
