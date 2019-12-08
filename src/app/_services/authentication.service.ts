45e486d4a46c32377ba20d0992a89db13dbb2544
jiyuan <jiyuan@motos-MacBook-puro.local>
2019-12-07 23:30:59 -0500

    change logout, add comment function

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -22,5 +22,7 @@ export class AuthenticationService {
     logout() {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
+        sessionStorage.removeItem('session_id');
+        
     }
 }
