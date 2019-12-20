ea88b9f760b4e6f2a9a84415569b35e67d757eb2
Rylie Gao <ryliegao@gmail.com>
2019-12-21 00:47:01 +0800

    fixed headers

--- src/app/_services/global.service.ts
+++ src/app/_services/global.service.ts
@@ -12,6 +12,6 @@ export class GlobalService {
     new HttpHeaders()
       .set('Content-Type', 'application/json')
       .set('Token', sessionStorage.getItem('session_id') || '')
-      .set('Authorization', localStorage.getItem('FBLoggedIn') || 'false')
+      .set('X-Api-Key', localStorage.getItem('FBLoggedIn') || 'false')
   ;
 }
